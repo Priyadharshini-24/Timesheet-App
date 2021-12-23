1f31b92ae442f4a4505f2098b777c6c9248480e2
Priyadharshini-24 <priyadharshini24.bca@gmail.com>
2021-12-23 22:01:31 +0530

    updated DB

--- sql/timesheet.sql
+++ sql/timesheet.sql
@@ -13,8 +13,8 @@ CONSTRAINT PK_USER_DETAILS PRIMARY KEY (USER_ID));
 
 DESC USER_DETAILS;
 
-DROP TABLE USER_DETAILS cascade constraints;
-DROP TABLE USER_DETAILS;
+--DROP TABLE USER_DETAILS cascade constraints;
+--DROP TABLE USER_DETAILS;
 
 INSERT INTO USER_DETAILS(FIRST_NAME,LAST_NAME,USER_NAME,PASSWORD,ROLE)VALUES('DHARSHAN','RAVI','dharshanravi@gmail.com','Dharshan03@','ADMIN');
 INSERT INTO USER_DETAILS(FIRST_NAME,LAST_NAME,USER_NAME,PASSWORD,ROLE)VALUES('PRIYA','RAVI','priyaravi@gmail.com','Priya24#','ADMIN');
@@ -38,7 +38,8 @@ CONSTRAINT PK_TASK_DETAILS PRIMARY KEY (TASK_ID));
 
 select * from task_details;
 DESC TASK_DETAILS;
-DROP TABLE TASK_DETAILS;
+
+--DROP TABLE TASK_DETAILS;
 
 CREATE TABLE TIMESHEETS(
 TIMESHEET_ID NUMBER GENERATED ALWAYS AS IDENTITY(START WITH 1 INCREMENT BY 1),
@@ -52,7 +53,7 @@ TIMESHEET_FOR_DATE DATE NOT NULL,
 TIMESHEET_UPDATE_DATE DATE DEFAULT SYSDATE NOT NULL,
 CONSTRAINT PK_TIMESHEETS PRIMARY KEY (TIMESHEET_ID)
 );
-
+select * from timesheets;
 DROP TABLE TIMESHEETS;
 DESC TIMESHEETS;
 
@@ -67,9 +68,30 @@ APPROVED_BY VARCHAR2(100),
 STATUS_UPDATE_DATE  DATE DEFAULT SYSDATE NOT NULL,
 CONSTRAINT PK_STATUS PRIMARY KEY(STATUS_ID)
 );
+select * from status;
+select * from status where status='rejected';
 
-drop table status;
+select * from status inner join timesheets on status.timesheet_id=timesheets.timesheet_id where status='rejected';
 
+select t.timesheet_id,t.task_id,t.timesheet_for_date,t.spend_time_hrs,t.comments,s.status
+from status s inner join timesheets t
+on s.timesheet_id=t.timesheet_id where status='rejected';
+
+select * from user_details u inner join task_details td on u.user_id=td.user_id inner join timesheets ts on td.task_id=ts.task_id inner join status s on ts.timesheet_id=s.timesheet_id;  
+
+select * from user_details u inner join task_details td on u.user_id=td.user_id inner join timesheets ts on td.task_id=ts.task_id inner join status s on ts.timesheet_id=s.timesheet_id where ts.timesheet_for_date='23-12-2021';
+
+select u.user_name,u.role,td.task_name,ts.timesheet_id,ts.spend_time_hrs,ts.timesheet_for_date,s.status,s.approved_by,s.status_update_date 
+from user_details u 
+inner join task_details td on u.user_id=td.user_id 
+inner join timesheets ts on td.task_id=ts.task_id 
+inner join status s on ts.timesheet_id=s.timesheet_id where ts.timesheet_for_date=to_date('23-12-2021','dd-MM-yyyy') and u.user_name='keerthanaguru@gmail.com'; 
+
+commit;
+--drop table status;
+
+select * from task_details;
+delete from status where status_id=5;
 
 DESC USER_DETAILS;
 DESC task_details;
